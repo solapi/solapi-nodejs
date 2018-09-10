b3f5da51bff8a5f6aef849de4e26c9f525b1f849
ksh-code <tjdgnsqn133@gmail.com>
2018-09-10 16:43:15 +0900

    버그 수정 및 예약 취소 추가

--- tests/test.js
+++ tests/test.js
@@ -145,6 +145,7 @@ describe('test', () => {
     })
     it('그릅 예약 (성공)', async () => {
       const group = new Group()
+      tempGroup = group
       await group.createGroup()
       const data = await group.addGroupMessage({
         to: getTo(),
@@ -154,6 +155,10 @@ describe('test', () => {
       })
       expect(data.errorCount).to.equal(0)
       expect(await group.setScheduledDate(new Date(Date.now() + (1000 * 60 * 60 * 10)).toISOString().split('.')[0].replace('T', ' '))).to.deep.equal({})
+    })
+    it('그릅 예약 취소 (성공)', async () => {
+      const data = await tempGroup.cancelScheduled()
+      expect(data).to.deep.equal({})
     })
     it('그릅 예약 (실패)', async () => {
       const group = new Group()
