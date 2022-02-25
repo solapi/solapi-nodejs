6a9ff6edd9c4db25970afe0207c4003175256fdf
Subin Lee <chandar.sblee@gmail.com>
2022-02-25 22:47:47 +0900

    SOLAPI 5.0.0-beta.17:
    * 불필요한 js 파일 제거
    * kakaoButton type에 linkMo, linkPc, linkAnd, linkIos 추가

--- src/models/kakaoOption.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class KakaoOption {
-    constructor() {
-        this.disableSms = false;
-        this.adFlag = false;
-    }
-}
-exports.default = KakaoOption;
