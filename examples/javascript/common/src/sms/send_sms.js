c371ae236ac50e72fca8409ffc8c02c65269b788
Subin Lee <chandar.sblee@gmail.com>
2022-05-17 18:35:46 +0900

    Remove wrong typo

--- examples/javascript/common/src/sms/send_sms.js
+++ examples/javascript/common/src/sms/send_sms.js
@@ -7,7 +7,7 @@ const messageService = new SolapiMessageService("ENTER_YOUR_API_KEY", "ENTER_YOU
 
 // 단일 발송 예제
 messageService.sendOne({
-  to: "수신번호",``
+  to: "수신번호",
   from: "계정에서 등록한 발신번호 입력",
   text: "한글 45자, 영자 90자 이하 입력되면 자동으로 SMS타입의 메시지가 발송됩니다."
 }).then(res => console.log(res));
