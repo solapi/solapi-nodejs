522d9a4a2ed7da7e3a2b5b5d0adef15e22f3f783
Subin Lee <chandar.sblee@gmail.com>
2025-03-05 16:46:55 +0900

    Add Next.js example

---
+++ examples/nextjs/tailwind.config.ts
@@ -0,0 +1,20 @@
+// tailwind.config.ts
+import type {Config} from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        pretendard: ['var(--font-pretendard)'],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
