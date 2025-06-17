efea92f5afcaddff85d349ae02bd3b9394ef02b3
Subin Lee <chandar.sblee@gmail.com>
2025-06-17 11:31:34 +0900

    fileToBase64.ts에서 URL 및 로컬 파일 경로를 처리하는 방식을 개선하고, 오류 처리를 명확하게 구분하여 반환하도록 수정함. 또한, 코드 포맷을 정리하고, 내부 유틸리티 함수를 추가하여 가독성을 향상시킴.

--- src/services/messages/messageService.ts
+++ src/services/messages/messageService.ts
@@ -105,7 +105,7 @@ export default class MessageService extends DefaultService {
       }
 
       const decodedConfig = Schema.decodeUnknownSync(sendRequestConfigSchema)(
-        requestConfigParameter,
+        requestConfigParameter ?? {},
       );
 
       const parameterObject = {
