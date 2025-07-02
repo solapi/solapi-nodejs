b5412bf469ed307e130b48f2af439fc42ca47acb
Subin Lee <chandar.sblee@gmail.com>
2025-07-02 10:18:09 +0900

    ESLint 설정을 개선하여 TypeScript 관련 플러그인과 규칙을 통합하고, tsconfig.json에서 테스트 디렉토리 이름을 수정함. 코드 품질을 향상시키기 위한 변경 사항 포함.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,57 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import {FlatCompat} from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslintEslintPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import tsdoc from 'eslint-plugin-tsdoc';
+import unusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
   {
-    ignores: ['jest.config.js', 'dist', 'coverage', 'docs', 'examples'],
+    ignores: ['dist/**/*', 'examples/**/*', 'debug/**/*', 'docs/**/*'],
   },
-);
+  ...compat.extends('plugin:@typescript-eslint/recommended'),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslintEslintPlugin,
+      tsdoc,
+      'unused-imports': unusedImports,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      parser: tsParser,
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+
+    rules: {
+      quotes: ['error', 'single'],
+      semi: [2, 'always'],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 1,
+
+      '@typescript-eslint/no-inferrable-types': [
+        'warn',
+        {
+          ignoreParameters: true,
+        },
+      ],
+
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'unused-imports/no-unused-imports': 'warn',
+    },
+  },
+];
